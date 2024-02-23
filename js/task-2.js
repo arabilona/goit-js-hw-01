826c68f158ab8612b4aa40ba2efc4dcd77742d8c
arabilona <113309074+arabilona@users.noreply.github.com>
2024-02-23 20:35:19 +0200

    add task 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,7 @@
 function getShippingMessage(country, price, deliveryFee) {
-    console.log(`Shipping to ${country} will cost ${price} * ${deliveryFee} credits`)
+    const totalPrice = price + deliveryFee;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
-
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
